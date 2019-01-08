4f760d6cc05c56381a07a7900ec2c618ffff1b05
mjbenefiel <michaeljbenefiel@gmail.com>
2019-01-08 15:05:47 -0600

    Updates

---
+++ precache-manifest.75a481b09d0e96ba34c5b924ed257c9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "3bbefffcbfdbf3c0be2e",
+    "url": "/memory-game/static/js/main.3bbefffc.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/memory-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "3bbefffcbfdbf3c0be2e",
+    "url": "/memory-game/static/css/main.d86f59dc.chunk.css"
+  },
+  {
+    "revision": "01c5271f678ae7927ab63e8e19d12614",
+    "url": "/memory-game/index.html"
+  }
+];
