384f014fbda23a93e5c97a4a18f7dac723c24736
mjbenefiel <michaeljbenefiel@gmail.com>
2019-01-12 12:16:06 -0600

    01122019 updated api, css, index.js

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./Title.css";
 
 const Title = props => (
+
     <div className="titlebox">
-        <h1>The Memory Game (in dev)</h1>
-        <p>Click on an image to earn points, but don't click on any more than once!</p>
+ 
+        <h1>Frenchie Memory Game</h1>
+        <p>Click on a frenchie to earn points, Game over if you click on the same frenchie more than once!</p>
+    
     </div>
+    
+    
 );
 export default Title;
